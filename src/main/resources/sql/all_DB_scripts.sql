f73241995d34e3b62030ea05d4a9ed12d46be451
blf <bolf@li.ru>
2023-01-29 22:44:59 +0300

    sql-scripts

---
+++ src/main/resources/sql/all_DB_scripts.sql
@@ -0,0 +1,44 @@
+create table course_schema.ext_lab2_genome_sequences (
+	id serial not null,
+	genome_id varchar not null, --идентификатор генома
+	seq_length int not null, --длина последовательности (шингла)
+	aa_sequence varchar not null --последовательность (шингл)
+);
+
+--функция, кот. рассчитывает показатель Жаккара на шинглах переданной длины
+create or replace function processGenSeqData(shingleLength int) returns
+setof numeric
+as
+$$
+		with gen1_len as (select distinct aa_sequence --уникальные шинглы первого генома
+							from ext_lab2_genome_sequences
+						  where genome_id = 'Genome_1-1' and seq_length = shingleLength),
+			 gen2_len as (select distinct aa_sequence --уникальные шинглы второго генома
+							from ext_lab2_genome_sequences
+							where genome_id = 'Genome_2-1' and seq_length = shingleLength),
+			 intersection_len2 as (select count(*) as cnt from gen1_len g1 --пере сечение уникальных шинглов
+			 				inner join gen2_len g2 on g1.aa_sequence = g2.aa_sequence),
+			 union_len2 as (select count(inner_.*) as cnt from (select * from gen1_len g1 --объединение уникальных шинглов
+									union all
+									select * from gen2_len) as inner_)
+		select
+		trunc(inters.cnt::numeric / un.cnt::numeric, 2) as res --отношение мощности пересечения к мощности объединения
+		from
+		intersection_len2 inters
+		left join union_len2 un on true
+$$
+language sql;
+
+--получение итоговой таблицы
+select 2 as k, processGenSeqData(2) as j
+union
+select 5, processGenSeqData(5)
+union
+select 9, processGenSeqData(9)
+
+--итоговая таблица:
+k|j   |
+-+----+
+2|0.50|
+5|0.50|
+9|0.38|
--- src/main/resources/sql/createTableExt_lab2_genome_sequences.sql
+++
@@ -1,6 +0,0 @@
-create table course_schema.ext_lab2_genome_sequences (
-	id serial not null,
-	genome_id varchar not null, --идентификатор генома
-	seq_length int not null, --длина последовательности (шингла)
-	aa_sequence varchar not null --последовательность (шингл)
-);
