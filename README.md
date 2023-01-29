8e5e79c33b2e88f1e4f88c03c1235936d15b32c0
blf <bolf@li.ru>
2023-01-29 23:26:52 +0300

    fixes

--- README.md
+++ README.md
@@ -6,7 +6,7 @@
 <meta charset="UTF-8"/>
 </head>
 <body>
-<table><tr><th>k (длина шингла)</th><th>j (знач.коэффициента)</th></tr><tr class="odd"><td>2</td><td>0,5</td></tr>
-<tr><td>5</td><td>0,5</td></tr>
-<tr class="odd"><td>9</td><td>0,38</td></tr>
+<table><tr><th>k (длина шингла)</th><th>j (знач.коэффициента)</th></tr><tr class="odd"><td>2</td><td>1</td></tr>
+<tr><td>5</td><td>1</td></tr>
+<tr class="odd"><td>9</td><td>0,63</td></tr>
 </table></body></html>
--- src/main/resources/sql/all_DB_scripts.sql
+++ src/main/resources/sql/all_DB_scripts.sql
@@ -6,19 +6,19 @@ create table course_schema.ext_lab2_genome_sequences (
 );
 
 --функция, кот. рассчитывает показатель Жаккара на шинглах переданной длины
-create or replace function processGenSeqData(shingleLength int) returns
-setof numeric
-as
-$$
+CREATE OR REPLACE FUNCTION course_schema.processgenseqdata(shinglelength integer)
+ RETURNS SETOF numeric
+ LANGUAGE sql
+AS $function$
 		with gen1_len as (select distinct aa_sequence --уникальные шинглы первого генома
 							from ext_lab2_genome_sequences
 						  where genome_id = 'Genome_1-1' and seq_length = shingleLength),
 			 gen2_len as (select distinct aa_sequence --уникальные шинглы второго генома
 							from ext_lab2_genome_sequences
 							where genome_id = 'Genome_2-1' and seq_length = shingleLength),
-			 intersection_len2 as (select count(*) as cnt from gen1_len g1 --пере сечение уникальных шинглов
+			 intersection_len2 as (select count(*) as cnt from gen1_len g1 --пересечение уникальных шинглов
 			 				inner join gen2_len g2 on g1.aa_sequence = g2.aa_sequence),
-			 union_len2 as (select count(inner_.*) as cnt from (select * from gen1_len g1 --объединение уникальных шинглов
+			 union_len2 as (select count(distinct inner_.*) as cnt from (select * from gen1_len g1 --объединение уникальных шинглов
 									union all
 									select * from gen2_len) as inner_)
 		select
@@ -26,8 +26,8 @@ $$
 		from
 		intersection_len2 inters
 		left join union_len2 un on true
-$$
-language sql;
+$function$
+;
 
 --получение итоговой таблицы
 select 2 as k, processGenSeqData(2) as j
@@ -37,8 +37,8 @@ union
 select 9, processGenSeqData(9)
 
 --итоговая таблица:
-k|j   |
+k|   j|
 -+----+
-2|0.50|
-5|0.50|
-9|0.38|
+2|1.00|
+5|1.00|
+9|0.63|
